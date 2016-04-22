c26b7979c6cef32c3dd540e2549112d20182f2b4
Eric Stephenson <ericb.stephenson@gmail.com>
2016-04-22 16:00:39 -0700

    fixed error in migration

--- db/migrate/20160422214310_create_blog_images.rb
+++ db/migrate/20160422214310_create_blog_images.rb
@@ -2,7 +2,7 @@ class CreateBlogImages < ActiveRecord::Migration
   def change
     create_table :blog_images do |t|
       t.string :image
-      t.references :user, foreign_key: true
+      t.references :blog_post, foreign_key: true
 
       t.timestamps null: false
     end
