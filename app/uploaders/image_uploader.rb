f561d17d5daf2cc8b92f8c64c98e8cafef840e86
Eric Stephenson <ericb.stephenson@gmail.com>
2016-06-07 13:18:00 -0700

    Adjust carrierwave storage options

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,7 +9,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
     storage :fog
-  else
+  elsif Rails.env.development? || Rails.env.test?
     storage :file
   end
 
