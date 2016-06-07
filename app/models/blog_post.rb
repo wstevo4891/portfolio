a265762617c4c4e0dbd099e3f6182eae9934b885
Eric Stephenson <ericb.stephenson@gmail.com>
2016-06-07 15:20:20 -0700

    Comment out mount_uploaders in blog_posts.rb

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,5 +1,5 @@
 class BlogPost < ActiveRecord::Base
 	validates :title, :date, :body, presence: true
 	mount_uploader :cover, ImageUploader
-	mount_uploaders :images, ImageUploader
+	# mount_uploaders :images, ImageUploader
 end
