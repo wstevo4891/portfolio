4d7a0361770b3d7396c3357b5dd51c4bd629110c
Eric Stephenson <ericb.stephenson@gmail.com>
2019-06-10 22:44:02 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,33 @@
 Rails.application.routes.draw do
-  resources :projects
   root 'home#index'
   
   devise_for :admins, 
              controllers: { sessions: "admins/sessions" }
 
-  resources :blog_images
-  resources :blog_posts
+  namespace :admin do
+    resources :projects
+    resources :posts
+  end
 
-  get '/work' => 'pages#work'
-  get '/about' => 'pages#about'
-  get 'pages/blog'
-  get '/contact' => 'pages#contact'
-  get '/dashboard' => 'pages#dashboard'
+  get '/work' => 'projects#index'
+  get '/work/:slug' => 'projects#show'
 
-  get 'work/bolt_network' => 'pages#bolt_network'
-  get 'work/baldwin' => 'pages#baldwin'
-  get 'work/griffen' => 'pages#griffen'
-  get 'work/smm' => 'pages#smm'
-  get 'work/suggestion_box' => 'pages#suggestion_box'
+  get '/about' => 'about#index'
+
+  get '/blog' => 'posts#index'
+  get '/blog/:slug' => 'posts#show'
+
+  get '/contact' => 'contact#index'
+
+  # get '/work' => 'pages#work'
+  # get '/about' => 'pages#about'
+  # get 'pages/blog'
+  # get '/contact' => 'pages#contact'
+  # get '/dashboard' => 'pages#dashboard'
+
+  # get 'work/bolt_network' => 'pages#bolt_network'
+  # get 'work/baldwin' => 'pages#baldwin'
+  # get 'work/griffen' => 'pages#griffen'
+  # get 'work/smm' => 'pages#smm'
+  # get 'work/suggestion_box' => 'pages#suggestion_box'
 end
