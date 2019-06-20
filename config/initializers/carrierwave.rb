3695f045e108515eee91a64ec5c16c4f289ea4ca
Eric Stephenson <ericb.stephenson@gmail.com>
2019-06-19 20:45:09 -0700

    Move config.storage = :fog line in Carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,12 +9,12 @@ end
 # Use AWS storage in production
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage = :fog
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV["AWS_ACCESS_KEY"],
       aws_secret_access_key: ENV["AWS_SECRET_KEY"]
     }
+    config.storage = :fog
     config.fog_directory = ENV["AWS_BUCKET"]
     config.fog_public = true
   end
